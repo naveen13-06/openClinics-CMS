ed54c0bfa86bc30b6002985c26dd224676459386
Naveen p <naveenoffl2002@gmail.com>
2023-04-24 10:15:47 +0530

    Lesson spelling

--- src/components/subjects.js
+++ src/components/subjects.js
@@ -2,9 +2,36 @@ const subjects=[
     {
         subject:'Pathology',
         lessons:[
-            'Cell Injury,Cell death and adaptations',
-            'Inflammation and repair',
-            'Hemodynamic Disorders'
+            'Cell Injury, Cell death and adaptations',
+    'Inflammation and repair',
+    'Hemodynamic Disorders',
+    'Genetic Disorders',
+    'Diseases of the Immune System',
+    'Neoplasia',
+    'Infectious Diseases',
+    'Environmental and Nutritional Diseases',
+    'Diseases of infancy and childhood',
+    'Blood vessels',
+    'The Heart',
+    'Diseases of WBCs',
+    'Red blood Cell and Bleeding Disorders','The Lung',
+    'Head and Neck',
+    'The Gastrointestinal Tract',
+    'Liver and Gallbladder',
+    'The Pancreas',
+    'The Kidney',
+    'The Lower Urinary Tract and Male Genital System',
+    'The Female Genital Tract',
+    'The Breast',
+    'The Endocrine System',
+    'The Skin',
+    'Bones, Joints, and Soft Tissue Tumors',
+    'Peripheral Nerves and Skeletal Muscles',
+    'The Central Nervous System',
+  'The Eye'
+
+
+
         ]
     },
     {
